151a1f3cf567b04bee4eb2f4c33aef3e69a518bc
nanjiangwill <willjiang2018@gmail.com>
2024-11-30 21:42:59 -0500

    Applied patch

--- cookiecutter/generate.py
+++ cookiecutter/generate.py
@@ -26,11 +26,28 @@ def is_copy_only_path(path, context):
         should be rendered or just copied.
     :param context: cookiecutter context.
     """
-    pass
+    copy_without_render = context.get("_copy_without_render", [])
+    for pattern in copy_without_render:
+        if fnmatch.fnmatch(path, pattern):
+            return True
+    return False
 
 def apply_overwrites_to_context(context, overwrite_context, *, in_dictionary_variable=False):
     """Modify the given context in place based on the overwrite_context."""
-    pass
+    for key, value in overwrite_context.items():
+        if isinstance(value, dict):
+            if key not in context:
+                context[key] = {}
+            apply_overwrites_to_context(context[key], value, in_dictionary_variable=True)
+        elif isinstance(value, list):
+            if key not in context:
+                context[key] = []
+            context[key].extend(value)
+        else:
+            if in_dictionary_variable and key not in context:
+                context[key] = value
+            elif not in_dictionary_variable:
+                context[key] = value
 
 def generate_context(context_file='cookiecutter.json', default_context=None, extra_context=None):
     """Generate the context for a Cookiecutter project template.
@@ -42,7 +59,22 @@ def generate_context(context_file='cookiecutter.json', default_context=None, ext
     :param default_context: Dictionary containing config to take into account.
     :param extra_context: Dictionary containing configuration overrides
     """
-    pass
+    context = {}
+    try:
+        with open(context_file, 'r') as file:
+            context = json.load(file, object_pairs_hook=OrderedDict)
+    except ValueError as e:
+        raise ContextDecodingException(context_file, str(e))
+
+    # Apply default context
+    if default_context:
+        apply_overwrites_to_context(context, default_context)
+
+    # Apply extra context
+    if extra_context:
+        apply_overwrites_to_context(context, extra_context)
+
+    return context
 
 def generate_file(project_dir, infile, context, env, skip_if_file_exists=False):
     """Render filename of infile as name of outfile, handle infile correctly.
@@ -65,7 +97,45 @@ def generate_file(project_dir, infile, context, env, skip_if_file_exists=False):
     :param context: Dict for populating the cookiecutter's variables.
     :param env: Jinja2 template execution environment.
     """
-    pass
+    logger.debug('Generating file %s', infile)
+
+    # Render the path to the output file (not including the root project dir)
+    outfile_tmpl = env.from_string(infile)
+    outfile = outfile_tmpl.render(**context)
+    outfile_path = os.path.join(project_dir, outfile)
+
+    # Create the directories to the outfile if they don't exist
+    dirname = os.path.dirname(outfile_path)
+    make_sure_path_exists(dirname)
+
+    if skip_if_file_exists and os.path.exists(outfile_path):
+        logger.debug('File %s already exists, skipping', outfile)
+        return
+
+    # Just copy over binary files without rendering
+    if is_binary(infile):
+        shutil.copyfile(infile, outfile_path)
+    else:
+        # Force fwd slashes on Windows for jinja template
+        infile_fwd_slashes = infile.replace(os.path.sep, '/')
+
+        # Render the file
+        try:
+            tmpl = env.get_template(infile_fwd_slashes)
+            rendered_file = tmpl.render(**context)
+        except UndefinedError as err:
+            msg = "Unable to create file '{}'".format(outfile)
+            raise UndefinedVariableInTemplate(msg, err, context)
+        except TemplateSyntaxError as err:
+            msg = "Unable to create file '{}'".format(outfile)
+            raise TemplateSyntaxError(msg, err.lineno, err.name, err.filename)
+
+        # Write the file
+        with open(outfile_path, 'w') as fh:
+            fh.write(rendered_file)
+
+    # Apply file permissions to output file
+    shutil.copymode(infile, outfile_path)
 
 def render_and_create_dir(dirname: str, context: dict, output_dir: 'os.PathLike[str]', environment: Environment, overwrite_if_exists: bool=False):
     """Render name of a directory, create the directory, return its path."""
@@ -97,4 +167,40 @@ def generate_files(repo_dir, context=None, output_dir='.', overwrite_if_exists=F
     :param keep_project_on_failure: If `True` keep generated project directory even when
         generation fails
     """
-    pass
+    template_dir = find_template(repo_dir)
+    logger.debug('Generating project from %s...', template_dir)
+    context = context or {}
+
+    unrendered_dir = os.path.split(template_dir)[1]
+    env = create_env_with_context(context)
+    project_dir = render_and_create_dir(unrendered_dir, context, output_dir, env, overwrite_if_exists)
+
+    # We want the Jinja path and the OS paths to match. Consequently, we'll:
+    #   + CD to the template folder
+    #   + Walk the path and generate the files
+    #   + CD back to where we started
+    with work_in(template_dir):
+        for root, dirs, files in os.walk('.'):
+            for d in dirs:
+                unrendered_dir = os.path.join(project_dir, root, d)
+                render_and_create_dir(unrendered_dir, context, output_dir, env, overwrite_if_exists)
+
+            for f in files:
+                infile = os.path.join(root, f)
+                if is_copy_only_path(infile, context):
+                    outfile_path = os.path.join(project_dir, infile)
+                    logger.debug('Copying %s to %s without rendering', infile, outfile_path)
+                    shutil.copyfile(infile, outfile_path)
+                    shutil.copymode(infile, outfile_path)
+                    continue
+                try:
+                    generate_file(project_dir, infile, context, env, skip_if_file_exists)
+                except UndefinedVariableInTemplate as err:
+                    if not keep_project_on_failure:
+                        rmtree(project_dir)
+                    raise
+
+    if accept_hooks:
+        _run_hook_from_repo_dir(repo_dir, 'post_gen_project', project_dir, context, delete_project_on_failure=not keep_project_on_failure)
+
+    return project_dir
